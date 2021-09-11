e102f6460ab8dd4f79580cfb95b8f1ad34eb3247
Erik Berkun-Drevnig <erik.b22@gmail.com>
2021-09-11 15:58:07 -0400

    Site skeleton

---
+++ README.md
@@ -0,0 +1,1 @@
+www.kolyisroel.ca
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  mode: "jit",
+  purge: {
+    content: ["./templates/**/*.html"],
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
